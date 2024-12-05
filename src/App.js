b661598efb3aa20e1da87f547917d67b24d9c31c
Paresh Esave <esaveparesh@gmail.com>
2024-12-05 17:20:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -36,12 +36,11 @@ const App = () => {
           )
           }
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
           }>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar/>
             </div>
-          </div>
 
           <div>
             <Routes>
@@ -72,7 +71,7 @@ const App = () => {
 
             </Routes>
           </div>
-
+          </div>
         </div>
       </BrowserRouter>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import Button from './Button'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const {activeMenu,setActiveMenu} = useStateContext()
+  const {activeMenu,setActiveMenu,screenSize} = useStateContext()
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black dark:bg-light-gray m-2'
@@ -18,8 +24,8 @@ const Sidebar = () => {
       {
         activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={()=>setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900' >
-          <SiShopware/> <span>Shoppy</span>
+          <Link to='/' onClick={handleCloseSideBar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900' >
+          <SiShopware/> <span>BlueFinn</span>
           </Link>
           <TooltipComponent content="Menu" position='BottomCenter'>
             <button type='button' onClick={()=>setActiveMenu((prevActiveMenu)=>!prevActiveMenu)} className='text-xl rounded-fullp-3 hover:bg-light-gray mt-4 block md-hidden '>
@@ -36,7 +42,7 @@ const Sidebar = () => {
                 </p>
               {
                 item.links.map((Link)=>(
-                  <NavLink to={`/${Link.name}`} key={Link.name} onClick={()=>{}} className={({isActive})=> isActive ? activeLink : normalLink}>
+                  <NavLink to={`/${Link.name}`} key={Link.name} onClick={handleCloseSideBar} className={({isActive})=> isActive ? activeLink : normalLink}>
                     {Link.icon}
                     <span className='capitalize'>
                       {Link.name}
